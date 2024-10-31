f62177342800558bc0cefa576b583a5bd20334a7
Masege Ratsoma <dynamitegod2@gmail.com>
2024-10-31 15:47:58 +0200

    Add get request

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 // TODO: create a default route that returns hello world
 //  HINT: get
 
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
 // This uses an environment variable to set the port. If the variable cannot be found,
 // it will default to 8080
 const port = process.env.PORT || 8080;
