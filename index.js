9b588a1a7c334848b148e3c6718089103ee53ce7
tyrell-snyders <tyrell.snyders@gmail.com>
2024-10-31 15:10:12 +0200

    indexjs

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// TODO: create a default route that returns hello world
+//  HINT: get
+
+// This uses an environment variable to set the port. If the variable cannot be found,
+// it will default to 8080
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
